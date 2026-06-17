0b47573a723f424ac511bacf89b69dddb04539de
徐浩顺 <xuhs0978@fingard.com>
2026-06-17 22:29:45 +0800

    fix: 相册选图添加 allowsEditing 显示确认按钮

--- app/camera.tsx
+++ app/camera.tsx
@@ -75,6 +75,8 @@ export default function CameraScreen() {
 
       const res = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
         quality: 0.8,
         base64: true,
       });
